2cccd4f69bfd9851bed3f83c780bc0676502c037
Brayan-Espina-Tramolao <71091071+Brayan-Espina-Tramolao@users.noreply.github.com>
2020-10-14 21:49:32 -0300

    Update Function3.py

--- Functions/Function3.py
+++ Functions/Function3.py
@@ -1,1 +1,5 @@
-
+Def interceptar(packet):
+  if packet['IP']['src'] == '172.17.0.2':
+    packet['MDNS']['id'] = '0x69'
+    print ("Interceptando tráfico MDNS, modificación cabecera MDNS id")
+    return packet

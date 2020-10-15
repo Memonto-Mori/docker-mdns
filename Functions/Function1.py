be1f768bcb610df3a30b9204c10c4972c06028ca
Brayan-Espina-Tramolao <71091071+Brayan-Espina-Tramolao@users.noreply.github.com>
2020-10-14 21:42:22 -0300

    Update Function1.py

--- Functions/Function1.py
+++ Functions/Function1.py
@@ -1,1 +1,12 @@
-
+Def interceptar (packet):
+  try:
+    if pakcet['IP']['src] == '172.17.0.2': #Dirección IP del cliente
+                    if packet['MDNS']['count.queries'] == 1: #Campo Question del msj MDNS
+                            print ('Campo a modificar "count.queries": ')
+                            print (packet['MDNS']['count.queries'])
+                            print ('Nuevo campo "count.queries":')
+                            packet['MDNS']['count.queries'] = 10
+                            print (packet['MDNS']['count.queries']) 
+                            return packet
+  except:
+                    return packet

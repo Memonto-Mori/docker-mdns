acfcb74ec0eca94fc272b19c4451eab6d5f2ab0e
Brayan-Espina-Tramolao <71091071+Brayan-Espina-Tramolao@users.noreply.github.com>
2020-10-14 21:58:39 -0300

    Update Function4.py

--- Functions/Function4.py
+++ Functions/Function4.py
@@ -1,7 +1,7 @@
 def interceptar(packet):
   try:
-    if packet['IP']['src'] == '172.17.0.3':
-      if packet['MDNS']['count.add_rr'] == 0:
+    if packet['IP']['src'] == '172.17.0.3': #Dirección IP del servidor MDNS
+      if packet['MDNS']['count.add_rr'] == 0: 
         print('Campo a cambiar "count.add_rr": ')
         print(packet['MDNS']['count.add_rr'])
         print('Nuevo campo "count.add_rr": ')

e2f456c0cb13fc1e41c2e495978f3335a9d2a6b5
Brayan-Espina-Tramolao <71091071+Brayan-Espina-Tramolao@users.noreply.github.com>
2020-10-14 21:56:34 -0300

    Update Function5.py

--- Functions/Function5.py
+++ Functions/Function5.py
@@ -1,1 +1,12 @@
-
+def interceptar(packet):
+  try:
+    if packet['IP']['src'] == '172.17.0.3':
+      if packet['MDNS']['count.auth_rr'] == 0:
+        print('Campo a cambiar "count.auth_rr": ')
+        print(packet['MDNS']['count.auth_rr'])
+        print('Nuevo campo "count.auth_rr": ')
+        packet['MDNS']['count.auth_rr'] = 100
+        print(packet['MDNS']['count.auth_rr'])
+        return packet
+  except:
+    return packet
